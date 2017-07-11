d86248dd7de4d50b17f340cadeeec87744b7f746
Seth Baughman <sbaughman1@me.com>
2017-07-11 11:48:11 -0400

    Render the user info and style it

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
 
+import './GithubUser.css'
+
 class GithubUser extends Component {
   constructor(props) {
     super(props)
 
-    this.fetchUserData()
+    this.state = {
+      user: {}
+    }
+
+    this.fetchUserData()  
   }
 
   fetchUserData() {
     fetch(`https://api.github.com/users/${this.props.match.params.username}`)
       .then(data => data.json())
-      .then(user => console.log(user))
+      .then(user => this.setState({ user }))
   }
 
   render() {
-    return <h1>Github User</h1>
+    const { user } = this.state
+    return (
+      <div className="github-user">
+        <img src={user.avatar_url} alt="user avatar" />
+        <h2>{user.login}</h2>
+        <h3>followers: {user.followers}</h3>
+        <h3>following: {user.following}</h3>
+        <h3>location: {user.location}</h3>
+        <a href={user.html_url} target="_">Link to {user.login}'s profile</a>
+      </div>
+    )
   }
 }
 
