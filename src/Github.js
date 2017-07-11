4e2fa9b19ed18fbfdbd0f89b71184ac1d8629c19
Seth Baughman <sbaughman1@me.com>
2017-07-11 11:02:36 -0400

    Link the form to state and style it

--- src/Github.js
+++ src/Github.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
+import './Github.css'
 
 class Github extends Component {
+  state = {
+    username: ''
+  }
+
+  handleChange = (ev) => {
+    this.setState({
+      username: ev.target.value
+    })
+  }
 
   render() {
-    return <h1>Github Component!</h1>
+    return (
+      <div className="github">
+        <img 
+          className="github-logo" 
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" 
+          alt="github logo" 
+        />
+        <form>
+          <div>
+            <input 
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <button>Look up Github user</button>
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
