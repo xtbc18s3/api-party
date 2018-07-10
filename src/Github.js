41f4ee723c476ae8a94f9539b36b04637db94d9a
Dave Strus <dave@getfretless.com>
2018-07-10 15:45:10 -0400

    Add Github component

---
+++ src/Github.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Github extends Component {
+  render() {
+    return (
+      <div className="Github">
+        GitHub (now a component)
+      </div>
+    )
+  }
+}
+
+export default Github
