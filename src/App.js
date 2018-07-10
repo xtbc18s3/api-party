5de7eb090dd97d865ec1e0d09859fe3b49c0b87c
Dave Strus <dave@getfretless.com>
2018-07-10 11:34:49 -0400

    Add a route for GitHub API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, NavLink } from 'react-router-dom'
+
+import './App.css'
 
 class App extends Component {
   render() {
@@ -9,9 +11,17 @@ class App extends Component {
           <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </header>
+
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+        </ul>
+
+        <Route path="/github" render={() => <h1>GitHub!</h1>} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
