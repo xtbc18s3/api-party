3da6c464171a6445dd48952e86d816ff3f2f429c
Dave Strus <dave@getfretless.com>
2018-07-10 12:10:18 -0400

    Get user data from GitHub

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react'
 import Github from './Github';
 
 class GithubUser extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: {}
+    }
+
+    this.fetchUserData()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.username !== this.props.match.params.username) {
+      this.fetchUserData()
+    }
+  }
+
+  fetchUserData = () => {
+    const { username } = this.props.match.params
+    fetch(`https://api.github.com/users/${username}`)
+      .then(response => response.json())
+      .then(user => this.setState({ user }))
+  }
+
   render() {
     return (
       <div className="GithubUser">
-        { this.props.match.params.username }
+        <img
+          src={ this.state.user.avatar_url }
+          alt={this.state.user.login}
+        />
       </div>
     )
   }
