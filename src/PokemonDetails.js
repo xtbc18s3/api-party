83489c0ab110f7560ea85b0838ebfbea4fdb863f
Dave Strus <dave@getfretless.com>
2018-07-10 16:24:36 -0400

    Load data from Pokemon API

--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
 
 class PokemonDetails extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pokemon: {},
+      error: null,
+    }
+
+    this.fetchPokemonData()
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.pokemon !== this.props.match.params.pokemon) {
+      this.fetchPokemonData()
+    }
+  }
+
+  fetchPokemonData = () => {
+    const { pokemon } = this.props.match.params
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+      .then(response => response.json())
+      .then(pokemon => this.setState({ pokemon }))
+  }
+
   render() {
     return (
       <div className="PokemonUser">
-        Hi, {this.props.match.params.pokemon}
+        <h2>{this.state.pokemon.name}</h2>
+        {
+          this.state.pokemon.sprites && (
+            <img
+              src={this.state.pokemon.sprites.front_default}
+              alt={this.state.pokemon.name}
+            />
+          )
+        }
       </div>
     )
   }
